1d54cbedc453d97c4b3b2ca0fe0f9cccff6fb466
zeeshanshamsuddeen <zeeshan.shamsu@gmail.com>
2017-02-01 21:43:27 +0530

    customer login

--- proj.py
+++ proj.py
@@ -1,20 +1,74 @@
 import sqlite3 
 from flask import Flask ,flash,session,render_template,redirect,escape, url_for,request
+from base64 import b64encode
 
 
 app = Flask(__name__)
 app.secret_key = 'any random string'
 
-
+#homepage
 @app.route('/')
 def homepage():
 	return render_template('homepage.html')
 
 
-#to access when location is selected from homepage
+@app.route('/test')
+def test():
+	conn=sqlite3.connect('images.db')
+	cur=conn.cursor()
+	cur.execute("SELECT * FROM images")
+	var=cur.fetchone()
+	image=b64encode(var[1])
+	conn.close()
+	return render_template('test.html',image=image,var=var)
+
+
+#page for displaying login for customers
+@app.route('/customer')
+def customer():
+	conn=sqlite3.connect('members.db')
+	cur=conn.cursor()
+	cur.execute("SELECT * FROM customers")
+	var=cur.fetchall()
+	conn.close()
+	return render_template('customer.html',var=var)
+	#from customer.html , goes to homepage_customer()
+
+#customer session is created
+@app.route('/customer_logged_in',methods=['GET','POST'])
+def customer_logged_in():
+	session['username']=request.form['username']              #session is a dictionery with username its key.. value is nm variable
+	return redirect(url_for('homepage_customer'))
+
+#customer session created after sign up
+@app.route('/customer_signed_up',methods=['GET','POST'])
+def customer_signed_up():
+	session['username']=request.form['username']             #session is a dictionery with username its key.. value is nm variable
+	password=request.form['password']
+	conn=sqlite3.connect('members.db')
+	cur=conn.cursor()
+	cur.execute("INSERT INTO customers(username,password) VALUES (?,?)",(session['username'],password))
+	cur.execute("CREATE TABLE {}(item text NOT NULL ,price INTEGER , qty TEXT ,total INTEGER , place TEXT ,rest TEXT)".format(session['username']))
+	conn.commit()
+	return redirect(url_for('homepage_customer'))
+
+#homepage of customer where restaurants are needed to be selected
+@app.route('/homepage_customer')
+def homepage_customer():
+	return render_template('homepage_customer.html')
+
+#when customer wants to logout
+@app.route('/customer_logout')
+def customer_logout():
+	session.pop('username', None)					#used to logout of current session
+	return redirect(url_for('homepage'))
+
+
+
+#to access when location is selected from homepage_customer
 @app.route('/<place>')			
 def location(place):
-	#checks which place is selected in homepage ie which databse
+	#checks which place is selected in homepage_customer ie which databse
 	if(place=='TLY'):
 		conn=sqlite3.connect('TLY.db')
 	elif(place=='KANNUR'):
@@ -62,6 +116,66 @@ def menu(place,rest):
 		return render_template('nomenu.html',place=place)
 	
 
+
+#when sorting is selected from menu
+@app.route('/<place>/menu/<rest>/<sort>')
+def menu_sort(place,rest,sort):
+	if(place=='TLY'):
+		conn=sqlite3.connect('TLY.db')
+	elif(place=='KANNUR'):
+		conn=sqlite3.connect('KANNUR.db')
+	else:
+		conn=sqlite3.connect('CALICUT.db')
+	conn.row_factory = sqlite3.Row
+	cur=conn.cursor()
+	if(sort=="nameasc"):
+		cur.execute("SELECT COUNT(*) FROM {}".format(rest))
+		var=cur.fetchone()
+		if(var['count(*)']>0):	#since row_factory=Row,col name is used
+			cur.execute("SELECT * FROM {} ORDER BY item ASC".format(rest))
+			var=cur.fetchall()
+			conn.close()
+			#<var> contains all items in table, each row is accessed and displayed by colname
+			return render_template('menu.html',var=var,place=place,rest=rest)
+		else:
+			return render_template('nomenu.html',place=place)	
+	elif(sort=="namedes"):
+		cur.execute("SELECT COUNT(*) FROM {}".format(rest))
+		var=cur.fetchone()
+		if(var['count(*)']>0):	#since row_factory=Row,col name is used
+			cur.execute("SELECT * FROM {} ORDER BY item DESC".format(rest))
+			var=cur.fetchall()
+			conn.close()
+			#<var> contains all items in table, each row is accessed and displayed by colname
+			return render_template('menu.html',var=var,place=place,rest=rest)
+		else:
+			return render_template('nomenu.html',place=place)	
+	elif(sort=="pricelh"):
+		cur.execute("SELECT COUNT(*) FROM {}".format(rest))
+		var=cur.fetchone()
+		if(var['count(*)']>0):	#since row_factory=Row,col name is used
+			cur.execute("SELECT * FROM {} ORDER BY price".format(rest))
+			var=cur.fetchall()
+			conn.close()
+			#<var> contains all items in table, each row is accessed and displayed by colname
+			return render_template('menu.html',var=var,place=place,rest=rest)
+		else:
+			return render_template('nomenu.html',place=place)	
+	elif(sort=="pricehl"):
+		cur.execute("SELECT COUNT(*) FROM {}".format(rest))
+		var=cur.fetchone()
+		if(var['count(*)']>0):	#since row_factory=Row,col name is used
+			cur.execute("SELECT * FROM {} ORDER BY price DESC".format(rest))
+			var=cur.fetchall()
+			conn.close()
+			#<var> contains all items in table, each row is accessed and displayed by colname
+			return render_template('menu.html',var=var,place=place,rest=rest)
+		else:
+			return render_template('nomenu.html',place=place)	
+
+
+
+
 #to submit quantity of item selected	
 @app.route('/quantity/<place>/<rest>/<item>/<price>',methods=['GET','POST'])
 def quantity(place,rest,item,price):
@@ -74,14 +188,14 @@ def quantity(place,rest,item,price):
 @app.route('/postquantity/<place>/<rest>/<item>/<price>',methods=['GET','POST'])
 def postquantity(place,rest,item,price):
 	print('postquantity')
-	conn=sqlite3.connect('bucket.db')
+	conn=sqlite3.connect('members.db')
 	cur=conn.cursor()
 	qty=request.form['qty']
 	var=int(price)*int(qty)
-	cur.execute("INSERT INTO CART(item,price,qty,total,place,rest) VALUES(?,?,?,?,?,?); " ,(item,price,qty,var,place,rest))
+	cur.execute("INSERT INTO {}(item,price,qty,total,place,rest) VALUES(?,?,?,?,?,?); ".format(session['username']) ,(item,price,qty,var,place,rest))
 	conn.commit()
 	conn.close()
-	return redirect(url_for('homepage'))
+	return redirect(url_for('homepage_customer'))
 
 
 
@@ -94,12 +208,12 @@ def postquantity(place,rest,item,price):
 #to show items in cart
 @app.route('/cartshow')
 def cartshow():
-	conn=sqlite3.connect('bucket.db')
+	conn=sqlite3.connect('members.db')
 	cur=conn.cursor()
-	cur.execute("SELECT COUNT(*) FROM CART")
+	cur.execute("SELECT COUNT(*) FROM {}".format(session['username']))
 	var1=cur.fetchone()
 	if(var1[0]>0):
-		cur.execute("SELECT * FROM CART")
+		cur.execute("SELECT * FROM {}".format(session['username']))
 		var=cur.fetchall()
 		conn.close()
 		return render_template('cartshow.html',var=var)
@@ -110,12 +224,12 @@ def cartshow():
 #to delete an <item> from cart
 @app.route('/cartremove/<item>/<place>/<rest>')
 def cartremove(item,place,rest):
-	conn=sqlite3.connect('bucket.db')
+	conn=sqlite3.connect('members.db')
 	cur=conn.cursor()
-	cur.execute("SELECT COUNT(*) FROM CART")
+	cur.execute("SELECT COUNT(*) FROM {}".format(session['username']))
 	var1=cur.fetchone()
 	if(var1[0]>0):
-		cur.execute("DELETE FROM CART WHERE item=? AND place=?",(item,place))
+		cur.execute("DELETE FROM {} WHERE item=? AND place=?;".format(session['username']),(item,place))
 		conn.commit()
 		conn.close()
 		return redirect(url_for('cartshow'))
@@ -125,12 +239,12 @@ def cartremove(item,place,rest):
 #to display total and proceed to pay
 @app.route('/cartpay')
 def cartpay():
-	conn=sqlite3.connect('bucket.db')
+	conn=sqlite3.connect('members.db')
 	cur=conn.cursor()
-	cur.execute("SELECT COUNT(*) FROM CART")
+	cur.execute("SELECT COUNT(*) FROM {}".format(session['username']))
 	var=cur.fetchone()
 	nitem=var[0]
-	cur.execute("SELECT SUM(total) FROM CART")
+	cur.execute("SELECT SUM(total) FROM {}".format(session['username']))
 	var=cur.fetchone()
 	ntotal=var[0]
 	return render_template('cartpay.html',nitem=nitem,ntotal=ntotal)
@@ -151,12 +265,12 @@ def paycash():
 #clear items in cart
 @app.route('/cartclear')
 def cartclear():
-	conn=sqlite3.connect('bucket.db')
+	conn=sqlite3.connect('members.db')
 	cur=conn.cursor()
-	cur.execute("DELETE FROM CART")
+	cur.execute("DELETE FROM {}".format(session['username']))
 	conn.commit()
 	conn.close()
-	return redirect(url_for('homepage'))
+	return redirect(url_for('homepage_customer'))
 
 
 
@@ -173,7 +287,7 @@ def cartclear():
 
 
 
-#to access when login is selected from homepage
+#to access when login is selected from homepage_customer
 @app.route('/login',methods=['GET','POST']	)
 def login():
 	return render_template('login.html')
@@ -226,9 +340,9 @@ def manager_edit(place,username,action):
 	if(action=="add"):
 		return render_template('manager_add.html',place=place,username=username)
 	elif(action=="delete"):
-		print('success')
 		return redirect(url_for('manager_delete',place=place,username=username))
-	
+	elif(action=="editprice"):
+		return redirect(url_for('manager_editprice',place=place,username=username))
 	
 
 
@@ -250,7 +364,8 @@ def manager_add(place,username):
 	item=request.form['item']
 	def1=request.form['def'] 
 	price=request.form['price']
-	cur.execute("INSERT INTO {}(item,def,price) VALUES(?,?,?); ".format(username) ,(item,def1,price))
+	category=request.form['category']
+	cur.execute("INSERT INTO {}(item,def,price,category) VALUES(?,?,?,?); ".format(username) ,(item,def1,price,category))
 	conn.commit()
 	conn.close()
 	return redirect(url_for('manager_menu',place=place,username=username))
@@ -300,15 +415,51 @@ def manager_delete_database(place,username,item):
 
 
 
+#to access when manager wants to edit price of items 
+@app.route('/manager_editprice/<place>/<username>')
+def manager_editprice(place,username):
+	if(place=='TLY'):
+		conn=sqlite3.connect('TLY.db')
+	elif(place=='KANNUR'):
+		conn=sqlite3.connect('KANNUR.db')
+	else:
+		conn=sqlite3.connect('CALICUT.db')	
+	cur=conn.cursor()
+	cur.execute("SELECT COUNT(*) FROM {}".format(username))
+	var1=cur.fetchone()
+	#if table is nonempty
+	if(var1[0]>0):
+		cur.execute("SELECT * FROM {}".format(username))
+		var=cur.fetchall()
+		conn.close()
+		return render_template('manager_editprice.html',place=place,username=username,var=var)
+	else:
+		return render_template("no_managermenu.html",place=place,username=username)
 
 
 
+#to edit price of item from database after manager selects edit price
+@app.route('/manager_editprice_newprice/<place>/<username>/<item>',methods=['GET','POST'])
+def manager_editprice_newprice(place,username,item):
+	return render_template('manager_editprice_newprice.html',place=place,username=username,item=item)
 
 
 
-@app.route('/login1')
-def login1():
-	return render_template('login1.html')
+#to enter new price in database
+@app.route('/manager_editprice_database/<place>/<username>/<item>',methods=['GET','POST'])
+def manager_editprice_database(place,username,item):
+	if(place=='TLY'):
+		conn=sqlite3.connect('TLY.db')
+	elif(place=='KANNUR'):
+		conn=sqlite3.connect('KANNUR.db')
+	else:
+		conn=sqlite3.connect('CALICUT.db')	
+	cur=conn.cursor()
+	price=request.form['price']
+	cur.execute("UPDATE {} SET price=? WHERE item=?".format(username),(price,item,))
+	conn.commit()
+	conn.close()
+	return redirect(url_for('manager_menu',place=place,username=username))
 
 
 
--- test.py
+++ test.py
@@ -1,13 +1,58 @@
+
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import glob
+import os
 import sqlite3
 
-conn=sqlite3.connect('bucket.db')
+# support both Python 2.x and 3.x
+try: buffer = buffer
+except NameError:
+    buffer = lambda x: x # on Python 3.x 'rb' mode already returns what we need
+
+def U(literal_string):
+    if hasattr(literal_string, 'decode'):
+        return literal_string.decode('utf-8') # source code encoding
+    return literal_string
+
+# open db
+conn = sqlite3.connect('images.db')
+conn.execute('''create table if not exists images (
+                    path unique not null, 
+                    image blob
+                )''')
+
+def genimages():
+    """Generate example images."""
+    for pngpath in glob.iglob(os.path.expanduser(U('E:\sree.jpg'))):
+        with open(pngpath, 'rb') as f:
+            yield pngpath, buffer(f.read())
+
+
+# print image paths
+for path, in conn.execute('select path from images'):
+    print(path)
+
 cur=conn.cursor()
-#cur.execute("ALTER TABLE CART ADD rest TEXT")
-#cur.execute("CREATE TABLE GREENLEAF(item text NOT NULL ,def TEXT , price INTEGER NOT NULL)")
-#cur.execute("INSERT INTO PHOTOS (PHOTO) VALUES ('sree.jpg')")
-cur.execute("SELECT * FROM CART")
-var=cur.fetchall()
+
+cur.execute("SELECT image FROM images")
+var=cur.fetchone()
 for x in var:
 	print(x)
-conn.commit()
-conn.close()	
+#conn=sqlite3.connect('test.db')
+#cur=conn.cursor()
+#cur.execute("PRAGMA table_info(CART)")
+#cur.execute("DROP table asd")
+#cur.execute("DESCRIBE KFC")
+#cur.execute("ALTER TABLE GREENLEAF ADD category TEXT")
+#cur.execute("CREATE TABLE zeeshan(item text NOT NULL ,price INTEGER , qty TEXT ,total INTEGER , place TEXT ,rest TEXT)")
+#cur.execute("CREATE TABLE customers(username text NOT NULL PRIMARY KEY , password text NOT NULL )")
+#cur.execute("INSERT INTO customers(username,password) VALUES ('zeeshan','zeeshan')")
+#cur.execute("DELETE FROM customers")
+#cur.execute("SELECT * FROM customers")
+#var=cur.fetchall()
+#for x in var:
+#	print(x)
+#conn.commit()
+#conn.close()	
